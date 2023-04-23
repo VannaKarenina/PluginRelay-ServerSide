a50de464718932189c7880c715249c87bb39b240
TudvasevMatvey <tudvasev.matvey@gmail.com>
2023-04-23 23:42:10 +0300

    User app impelemnted

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,8 +1,10 @@
 import {Module} from '@nestjs/common';
 import {TestModule} from "@mmh/gateway/apps/test";
+import {UserModule} from "@mmh/gateway/apps/user/user.module";
 @Module({
   imports: [
-    TestModule
+    TestModule,
+    UserModule
   ]
 })
 export class AppModule {}
