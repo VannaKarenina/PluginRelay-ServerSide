7ea65d83780eba4d25c899ee3f05ab5435787f39
TudvasevMatvey <tudvasev.matvey@gmail.com>
2023-05-08 23:42:13 +0300

    Files refactor in staged mode

---
+++ apps/user/src/configs/mikro-orm.config.ts
@@ -0,0 +1,39 @@
+import {
+  EntityCaseNamingStrategy,
+  NullCacheAdapter,
+  Options,
+} from '@mikro-orm/core';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import {PostgreSqlDriver} from "@mikro-orm/postgresql";
+import {AccountEntity} from "@mmh/entities";
+import {ProjectsEntity} from "@mmh/entities/projects/projects.entity";
+
+const { DB_HOST, DB_PORT, DB_PASSWORD, DB_USER, DB_DATABASE } = process.env;
+
+export const MikroOrmConfig: Options = {
+  driver: PostgreSqlDriver,
+  entities: [
+    AccountEntity,
+    ProjectsEntity
+  ],
+  dbName: DB_DATABASE,
+  host: DB_HOST,
+  port: parseInt(DB_PORT),
+  user: DB_USER,
+  password: DB_PASSWORD,
+  highlighter: new SqlHighlighter(),
+  debug: process.env.NODE_ENV !== 'production',
+  baseDir: process.cwd(),
+  cache: { enabled: false },
+  validateRequired: false,
+  namingStrategy: EntityCaseNamingStrategy,
+  resultCache: { adapter: NullCacheAdapter },
+  autoJoinOneToOneOwner: false,
+  migrations: {
+    tableName: 'migrations',
+    path: '../database/migrations',
+  },
+  discovery: {
+    warnWhenNoEntities: false
+  }
+};
