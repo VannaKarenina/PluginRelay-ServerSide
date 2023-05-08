5672385b2087a696738bb34d860b69a4b77d2f26
TudvasevMatvey <tudvasev.matvey@gmail.com>
2023-05-08 23:48:41 +0300

    Projects module register

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -3,12 +3,14 @@
   import {StorageModule} from "@mmh/gateway/apps/storage";
   import {SharedMoleculerModule} from "@mmh/gateway/shared";
   import {AuthModule} from "@mmh/gateway/shared";
+  import {ProjectsModule} from "@mmh/gateway/apps/projects/projects.module";
   @Module({
     imports: [
       UserModule,
       StorageModule,
       SharedMoleculerModule,
-      AuthModule
+      AuthModule,
+      ProjectsModule
     ],
   })
   export class AppModule {
